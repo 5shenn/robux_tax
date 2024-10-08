93f5fa9e948258824195bda6e65113d82d86024b
shen <142518601+5shenn@users.noreply.github.com>
2024-10-08 22:54:14 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    document.getElementById('cost').value = 'R ';
+};
+
+function calculateEarnedAmount() {
+    const amountInput = document.getElementById('cost').value.replace(/R |,/g, '').trim(); 
+    let totalAmount = parseFloat(amountInput);
+    
+    if (!isNaN(totalAmount)) {
+        const earnedAmount = (totalAmount * 0.70); 
+        document.getElementById('amount').value = 'R ' + Math.round(earnedAmount).toLocaleString(); 
+    } else {
+        document.getElementById('amount').value = 'Invalid input'; 
+    }
+}
+
+
+function calculateTotalAmount() {
+    const amountInput = document.getElementById('cost').value.replace(/R |,/g, '').trim(); 
+    let earnedAmount = parseFloat(amountInput);
+    
+    if (!isNaN(earnedAmount)) {
+        const totalAmount = (earnedAmount / 0.70); 
+        document.getElementById('amount').value = 'R ' + Math.round(totalAmount).toLocaleString();
+    } else {
+        document.getElementById('amount').value = 'Invalid input'; 
+    }
+}
+
+function formatNumberWithCommas(input) {
+    let value = input.value.replace(/R |,/g, '').trim();
+
+    if (!isNaN(value) && value.length > 0) {
+        
+        input.value = 'R ' + Number(value).toLocaleString();
+    } else {
+        input.value = 'R '; 
+    }
+}
